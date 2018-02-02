a8b5cd5b6318b3bb1484ed7ac200ea710090c583
Sophie Kane <skane0207@gmail.com>
2018-02-02 00:47:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,38 +9,6 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
-
-var itemPrice = Math.floor((Math.random() * 100) + 1);
-var itemName = {[item]: itemPrice};
-
-cart.push(itemName);
-
-console.log(itemName + " has been added to your cart."); 
-
-return cart;
-  
-}
-
-function viewCart() {
-  var message = "In your cart, you have ";
- 
-  if (cart.length === 0) {
-   
-                return "Your shopping cart is empty.";
-   
-      }  else {  
-  
-          for ( var num = 1; num <= cart.length; num+= 1 ) {
-     
-              message += cart[num] + ', '
-     
-                           }
-           return message
- }
- 
- 
-}
 
 function total() {
   // write your code here
